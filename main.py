54e04f7d48801ae9508ec3a4e88737b7f76dfd82
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2022-11-05 10:58:45 -0400

    Path Parameters and use of Enum

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+# By inheriting from str the API docs will be able to know that the values must be of 
+# type string and will be able to render correctly.
+class ModelName(str, Enum):
+    # These are machine learning models names
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()  # Lanzar la app con el comando: uvicorn main:app --reload
 
@@ -22,3 +34,25 @@ async def testing_int():
 @app.get("/testing/str", tags=["testing"])
 async def testing_str():
     return "testing"
+
+
+@app.get("/path/parameters/{path_parameter}")
+async def using_path_parameter(path_parameter: str):
+    return {"path_parameter": path_parameter}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name = ModelName):
+    # Learning at the same time how to interact with enums in python
+    # I was using "is" for the compare but it wasn't working, "==" did
+    # "is" started working when I used model_name.alexnet.value
+    if model_name is ModelName.alexnet.value:  
+        return {"model_name": model_name, "message": "Deep Learning Model!"}
+    elif model_name == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+
+
